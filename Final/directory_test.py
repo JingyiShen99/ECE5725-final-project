10de202f17648f69d020dd7842f1b0828301d3bc
Yunhe Shao <yunhes@uci.edu>
2021-12-10 12:49:38 -0500

    bug fix

--- Final/directory_test.py
+++ Final/directory_test.py
@@ -17,35 +17,40 @@ def log_request(path, complete_flag):
             j = 0
             local_result_list = []
             searchlines = f.readlines()
+            last_line = searchlines[-1]
             image_path = searchlines[0].split(':')[0]
-            predictions_num = int(searchlines[1]) - 1
-            for i in range(predictions_num):
-                box = searchlines[1 + 1 + i].split(',')
-                i += 1
-            for j in range(predictions_num):
-                print(searchlines[2 + i + 1 + j])
-                print(2 + i + 1 + j)
-                local_result_list.append(str(searchlines[2 + i + 1 + j].split(':')[0]))
-                if complete_flag:
-                    if local_result_list[j] == 'fedex':
-                        result_list[image_path].append("fedex")
-                    elif local_result_list[j] == 'dhl':
-                        result_list[image_path].append("dhl")
-                    elif local_result_list[j] == 'ups':
-                        result_list[image_path].append("ups")
-                    elif local_result_list[j] == 'food delivery':
-                        result_list[image_path].append("food")
-                    list_pointer += 1
-                else:
+            print(image_path)
 
-                    print(box)
-                j += 1
+            if searchlines[1] != 0:
+                predictions_num = int(searchlines[1]) - 1
+                for i in range(predictions_num):
+                    box = searchlines[1 + 1 + i].split(',')
+                    i += 1
+                for j in range(predictions_num):
+                    local_result_list.append(str(searchlines[2 + i + 1 + j].split(':')[0]))
+                    if complete_flag:
+                        if local_result_list[j] == 'fedex':
+                            result_list[image_path].append("fedex")
+                        elif local_result_list[j] == 'dhl':
+                            result_list[image_path].append("dhl")
+                        elif local_result_list[j] == 'ups':
+                            result_list[image_path].append("ups")
+                        elif local_result_list[j] == 'food delivery':
+                            result_list[image_path].append("food")
+                        list_pointer += 1
+                    else:
+
+                        print(box)
+                    if searchlines[2 + i + 1 + j] is last_line:
+                        break
+                    j += 1
+
         list_pointer += 1
-        print(image_path)
         print(result_list)
+
             
     f.close()
 
 
 
-log_request("/mnt/g/research/ECE-5725-group8/sample_out/",1)
+log_request("/home/pi/PiImage/OutTxt",1)
--- Final/reply.py
+++ Final/reply.py
@@ -66,35 +66,37 @@ def log_request(path, complete_flag):
             j = 0
             local_result_list = []
             searchlines = f.readlines()
+            last_line = searchlines[-1]
             image_path = searchlines[0].split(':')[0]
-            predictions_num = int(searchlines[1]) - 1
-            for i in range(predictions_num):
-                box = searchlines[1 + 1 + i].split(',')
-                i += 1
-            for j in range(predictions_num):
-                print(searchlines[2 + i + 1 + j])
-                print(2 + i + 1 + j)
-                local_result_list.append(str(searchlines[2 + i + 1 + j].split(':')[0]))
-                if complete_flag:
-                    if local_result_list[j] == 'fedex':
-                        result_list[image_path].append("fedex")
-                    elif local_result_list[j] == 'dhl':
-                        result_list[image_path].append("dhl")
-                    elif local_result_list[j] == 'ups':
-                        result_list[image_path].append("ups")
-                    elif local_result_list[j] == 'food delivery':
-                        result_list[image_path].append("food")
-                    list_pointer += 1
-                else:
-                    pass
-                j += 1
+            print(image_path)
+
+            if searchlines[1] != 0:
+                predictions_num = int(searchlines[1]) - 1
+                for i in range(predictions_num):
+                    box = searchlines[1 + 1 + i].split(',')
+                    i += 1
+                for j in range(predictions_num):
+                    local_result_list.append(str(searchlines[2 + i + 1 + j].split(':')[0]))
+                    if complete_flag:
+                        if local_result_list[j] == 'fedex':
+                            result_list[image_path].append("fedex")
+                        elif local_result_list[j] == 'dhl':
+                            result_list[image_path].append("dhl")
+                        elif local_result_list[j] == 'ups':
+                            result_list[image_path].append("ups")
+                        elif local_result_list[j] == 'food delivery':
+                            result_list[image_path].append("food")
+                        list_pointer += 1
+                    else:
+
+                        print(box)
+                    if searchlines[2 + i + 1 + j] is last_line:
+                        break
+                    j += 1
+
         list_pointer += 1
-        print(image_path)
-    print(result_list)
-    if complete_flag:
-        return result_list
-    else:
-        return local_result_list
+        print(result_list)
+
             
     f.close()
 	
